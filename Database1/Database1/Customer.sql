aa8c2abe557eed0e5f1cc464d33c377d3dc2de1d
sathya177 <47978849+sathya177@users.noreply.github.com>
2019-05-27 22:18:42 +0000

    Update Customer.sql

--- Database1/Database1/Customer.sql
+++ Database1/Database1/Customer.sql
@@ -5,6 +5,7 @@
     [Address] NVARCHAR(50) NULL, 
     [ZipCode] INT NULL, 
     [State] NVARCHAR(50) NULL,
-	[Country] NVARCHAR(50) NULL
+	[Country] NVARCHAR(50) NULL,
+	[Active] VARCHAR(2) NULL
 
 )
