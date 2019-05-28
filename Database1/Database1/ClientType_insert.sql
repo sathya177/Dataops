965be947383a29738bdf831c789058f8768579cb
sathya177 <47978849+sathya177@users.noreply.github.com>
2019-05-28 04:18:15 +0000

    Create ClientType_insert.sql

---
+++ Database1/Database1/ClientType_insert.sql
@@ -0,0 +1,7 @@
+SET IDENTITY_INSERT [dbo].[ClientType] ON
+INSERT INTO [dbo].[ClientType] ([ClientTypeId], [Name], [Detail]) VALUES (1, N'Private Client', NULL)
+INSERT INTO [dbo].[ClientType] ([ClientTypeId], [Name], [Detail]) VALUES (2, N'Retail Client', NULL)
+INSERT INTO [dbo].[ClientType] ([ClientTypeId], [Name], [Detail]) VALUES (3, N'Official Client', NULL)
+INSERT INTO [dbo].[ClientType] ([ClientTypeId], [Name], [Detail]) VALUES (4, N'Global Client', NULL)
+INSERT INTO [dbo].[ClientType] ([ClientTypeId], [Name], [Detail]) VALUES (5, N'Special Client', NULL)
+SET IDENTITY_INSERT [dbo].[ClientType] OFF
